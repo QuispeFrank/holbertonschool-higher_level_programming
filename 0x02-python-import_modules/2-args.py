0d68ec2ab18aa9fa26e74de501d08dcfc04b4e52
Frank <frankchristopher@gmail.com>
2022-04-28 19:31:53 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    # tamaño del argv
     args = len(sys.argv) - 1
     if args == 0:
         print(f"{args} arguments.")
+    elif args == 1:
+        print(f"{args} argument:")
     else:
         print(f"{args} arguments:")
 
