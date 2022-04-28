e6840ed5612feb76b274ff3b4fa3dcea5d662833
Frank <frankchristopher@gmail.com>
2022-04-28 19:27:18 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,10 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    print(f"{args} arguments.")
+    if args == 0:
+        print(f"{args} arguments.")
+    else:
+        print(f"{args} arguments:")
+
     for i in range(1, args + 1):
         print(f"{i}: {sys.argv[i]}")
