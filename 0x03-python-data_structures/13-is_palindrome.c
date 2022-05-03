60971aae5566b6e62c2f08526f8b104397ee41ca
Frank <frankchristopher@gmail.com>
2022-05-03 03:38:52 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,88 +1,39 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the
- * number of elements in a linked list_t list
- * @h: head of the linked list
- * Return: a function that returns the number
- * of elements in a linked listint_t list.
- */
-
-size_t listint_len(const listint_t *h)
-{
-	size_t counter = 0;
-
-	for (; h; counter++)
-	h = h->next;
-
-	return (counter);
-}
-
-/**
- * node_at_index -  a function that returns the nth
- * node of a listint_t linked list.
+ * check_palindrome - check_palindrome
+ * @head: double pointer
+ * @last: pointer
  *
- * @head: a pointer to the head of the single
- * linked list.
- * @index: the index of the node.
- * Return: @n data.
+ * Return: 1 or 0
  */
-
-listint_t *node_at_index(listint_t *head, unsigned int index)
+int check_palindrome(listint_t **head, listint_t *last)
 {
-	listint_t *node = NULL;
-
-	/* validacion de nodos nulos */
-	if (!head)
-		return (NULL);
-	/* si solo existe un nodo */
-	if (!index)
-		return (head);
-
-	node = head;
-	/* si existen mas nodos */
-	while (index--)
+	/* condicion de break */
+	if (last->next != NULL)
 	{
-		if (!node)
-			break;
-		node = node->next;
+		if (check_palindrome(head, last->next) == 0)
+			return (0);
 	}
-	return (node);
+	if ((*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
+
 /**
- * is_palindrome - checks if a singly linked list
- * is a palindrome.
- * @head: head of the single linked list.
+ * is_palindrome - is_palindrome
+ * @head: double pointer
  *
- * Return: 1 if it's palindrome, 0 else.
+ * Return: 0, 1
  */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *left_node = NULL;
-	listint_t *right_node = NULL;
-	int list_len, i = 0, j;
-	int palindrome = 0;
-
-	/* un solo elemento es palindrome */
 	if (head == NULL || *head == NULL)
-		return (1);
+		return (0);
 
-	list_len = listint_len(*head);
-	if (list_len == 1)
-		return (1);
-
-	j = list_len - 1;
-	for (i = 0; i < list_len / 2; i++, j--)
-	{
-		left_node = node_at_index(*head, i);
-		right_node = node_at_index(*head, j);
-
-		if (left_node->n == right_node->n)
-		{
-			palindrome = 1;
-			continue;
-		}
-		palindrome = 0;
-	}
-	return (palindrome);
+	return (check_palindrome(head, *head));
 }
