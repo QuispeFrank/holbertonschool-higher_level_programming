f007f63da3a5602e2eca48d605a4c1964e27030c
Frank <frankchristopher@gmail.com>
2022-05-04 22:55:11 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict) is False:
+    if isinstance(a_dictionary, dict) or len(a_dictionary) is False:
         return None
 
     max_value = max([a_dictionary[item] for item in a_dictionary])
