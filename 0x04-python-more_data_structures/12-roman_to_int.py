6c59ca9960258decf109ef2fb151da00e4bc0181
Frank <frankchristopher@gmail.com>
2022-05-05 01:07:36 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False:
+        return 0
+
     r_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if len(roman_string) == 0:
         return None
