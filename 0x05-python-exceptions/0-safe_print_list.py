7f4230fd6520d522a669871331eaec6458e65b9d
FranQ <frankchristopherquispe@gmail.com>
2022-05-16 23:53:48 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+def safe_print_list(my_list=[], x=0):
+
+    items = 0
+    for idx in range(x):
+        try:
+            print(f"{my_list[idx]}", end="")
+            items += 1
+
+        except IndexError:
+            break
+
+    print()
+    return items
