c4fc2e630e38cb26aea2e09efe0c6c60929754d0
FranQ <frankchristopherquispe@gmail.com>
2022-05-17 00:46:59 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[idx]}", end="")
             items += 1
-
         except IndexError:
             break
+    print("")
 
-    print()
-    return (items)
+    return items
