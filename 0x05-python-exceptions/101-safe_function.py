da7a8c21d57a504286abeb16e704224f2b5a7384
FranQ <frankchristopherquispe@gmail.com>
2022-05-17 03:53:47 +0000

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return(fct(*args))
+    except BaseException as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return None
