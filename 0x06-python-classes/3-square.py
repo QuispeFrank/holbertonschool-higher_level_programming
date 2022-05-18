de1970fb2d4e7f006e29f0e448f7b5e30eec0de4
FranQ <frankchristopherquispe@gmail.com>
2022-05-18 02:23:06 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A square is here"""
+
+
+class Square:
+    """I'm a square class.
+
+    Attributes:
+        __size: square side size.
+    """
+
+    def __init__(self, size=0):
+        """Inits Square with size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the Square's area"""
+        return (self.__size * self.__size)
