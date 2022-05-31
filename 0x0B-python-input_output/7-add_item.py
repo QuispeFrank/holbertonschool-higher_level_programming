808d075853a238620cc36c23bff7a1e7cc415aaa
FranQ <frankchristopherquispe@gmail.com>
2022-05-31 23:56:41 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" function that creates an Object from a “JSON file """
+""" function that creates an Object from a JSON file """
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
