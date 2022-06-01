db7199ea47c15aefcbdb6cb73975f8b2f88b08c3
FranQ <frankchristopherquispe@gmail.com>
2022-06-01 02:17:10 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student():
+    """ I'm a student class :) """
+    def __init__(self, first_name, last_name, age):
+        """ inits a student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ F. that returns student's information """
+        if type(attrs) != list:
+            return self.__dict__
+        d = {k: v for k, v in self.__dict__.items() for a in attrs if a == k}
+        return (d)
