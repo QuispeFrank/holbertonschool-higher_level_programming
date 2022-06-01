b91e7183e0cb0a63dc2b5f79ac3f551ccb64e457
FranQ <frankchristopherquispe@gmail.com>
2022-06-01 00:35:57 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
+    new_lista = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    save_to_json_file([], 'add_item.json')
+finally:
     size = len(sys.argv)
     lista = []
     for i in range(1, size):
@@ -13,6 +17,3 @@ try:
     new_lista = load_from_json_file('add_item.json')
     new_lista += lista
     save_to_json_file(new_lista, 'add_item.json')
-
-except FileNotFoundError:
-    save_to_json_file([], 'add_item.json')
