4ccbc952234c4b07cbf07e9624de6c69f69e7827
FranQ <frankchristopherquispe@gmail.com>
2022-06-01 01:08:51 +0000

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,11 @@
 class Student():
     """ I'm a student class :) """
     def __init__(self, first_name, last_name, age):
+        """ inits a student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ F. that returns student's information """
         return (self.__dict__)
