420907c4799da9220a40074825a37c5ee8a175ea
FranQ <frankchristopherquispe@gmail.com>
2022-06-01 15:14:10 +0000

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Function that returns a list of lists
-of integers representing the Pascal’s triangle of"""
+""" Pascal's Triangle """
 
 
 def pascal_triangle(num):
-    """Returns a list of integers
-        n : number of triangle size
+    """ returns a list of lists of integers representing
+        the Pascal’s triangle of n.
     """
+    if num <= 0:
+        return []
+
     p = [[1]]
     for r in range(1, num):
         p.append([1] + [p[r-1][n-1] + p[r-1][n] for n in range(1, r)] + [1])
