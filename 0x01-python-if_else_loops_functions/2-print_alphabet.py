8fa8b99a5fabeb9ffc9f4cbc98ae615e75bfa775
FranQ <frankchristopherquispe@gmail.com>
2022-06-13 17:38:38 +0000

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" I sometimes suffer from insomnia. And when I can't
+    fall asleep, I play what I call the alphabet game.
+"""
+
+
 for letter in range(ord('a'), ord('z') + 1):
     print(chr(letter), end='')
