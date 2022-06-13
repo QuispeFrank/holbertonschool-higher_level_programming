310300094bcd1ddca28b365dfafab315cb83a787
FranQ <frankchristopherquispe@gmail.com>
2022-06-13 22:18:57 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for dec in range(0, 8):
-    for uni in range(0, 10):
-        if uni > dec:
-            print("{}{}, ".format(dec, uni), end="")
-print(89)
+""" Inventing is a combination of brains and materials.
+    The more brains you use, the less material you need
+"""
+
+
+for dec in range(0, 9):
+    for uni in range(dec + 1, 10):
+        print(f'{dec}{uni}', end=', ' if dec != 8 else '\n')
