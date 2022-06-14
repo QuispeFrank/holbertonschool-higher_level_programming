5976397bca938cbb362b8d34af25191c2364dca2
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 18:34:27 +0000

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" Replace element """
+
+
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return(my_list)
-    my_list[idx] = element
+    """ replaces an element of a list at a
+        specific position (like in C)
+    """
+    if idx in range(0, len(my_list)):
+        my_list[idx] = element
     return(my_list)
