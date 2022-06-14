d3ae2f17bfc602b9958f2d5e34a9fcd453fa0c12
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 23:43:16 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" More returns! """
+
+
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if len(sentence) != 0 else None)
+    """ returns a tuple with the length of a
+        string and its first character
+    """
+    return(len(sentence), sentence[0] if sentence else None)
