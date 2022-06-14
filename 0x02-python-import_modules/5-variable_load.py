2234c373378afb070e11e16df4199552ea893063
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 03:40:50 +0000

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Everything can be imported """
+
+
 if __name__ == "__main__":
-    import variable_load_5
-    print(variable_load_5.a)
+    from variable_load_5 import a
+    print(a)
