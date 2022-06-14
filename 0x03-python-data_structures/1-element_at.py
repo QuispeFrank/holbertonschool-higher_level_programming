01841e49fa2994d87810f3facbf0b38a72536ae0
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 18:20:00 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" Secure access to an element in a list """
+
+
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return(None)
-    return(my_list[idx])
+    """ retrieves an element from a list like in C """
+    return(my_list[idx] if idx in range(0, len(my_list)) else None)
