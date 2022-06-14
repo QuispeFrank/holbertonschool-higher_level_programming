90cfea2efb94e72c83e21c9c859660225c243921
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 02:08:51 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" Import a simple function from a simple file """
+
+
 if __name__ == "__main__":
-    import add_0 as sum
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, sum.add(a, b)))
+    import add_0 as module
+    print(f'1 + 2 = {module.add(1, 2)}')
