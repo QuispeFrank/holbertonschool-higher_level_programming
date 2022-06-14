078c6715cd51febc3b424460eba91330b8493081
FranQ <frankchristopherquispe@gmail.com>
2022-06-14 02:51:44 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" Infinite addition """
+
+
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print(sum)
+    from sys import argv
+    print(sum([int(argv[n]) for n in range(1, len(argv))]))
