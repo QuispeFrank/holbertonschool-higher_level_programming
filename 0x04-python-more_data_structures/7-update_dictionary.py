2a820b90f0d1a72709c1ea8a84cd51e3f388ef7a
FranQ <frankchristopherquispe@gmail.com>
2022-06-15 15:34:29 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Update dictionary """
+
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    """ replaces or adds key/value in a dictionary """
+    a_dictionary.update({key: value})
     return a_dictionary
