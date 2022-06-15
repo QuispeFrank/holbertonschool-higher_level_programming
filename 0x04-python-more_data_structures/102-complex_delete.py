74d1d892c50c903100a335768bd88a281544ab2b
FranQ <frankchristopherquispe@gmail.com>
2022-06-15 20:26:28 +0000

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" Delete by value """
+
+
 def complex_delete(a_dictionary, value):
-    b_dictionary = a_dictionary.copy()
-    for key in b_dictionary:
-        if b_dictionary[key] == value:
-            del a_dictionary[key]
+    """ deletes keys with a specific value in a dictionary """
+    list_to_delete = [k for k, v in a_dictionary.items() if v == value]
+    for key in list_to_delete:
+        del a_dictionary[key]
     return a_dictionary
