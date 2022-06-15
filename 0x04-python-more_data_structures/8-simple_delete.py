6677ded6e76433accc9b417981f5625d062defa5
FranQ <frankchristopherquispe@gmail.com>
2022-06-15 15:42:24 +0000

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Simple delete by key """
+
+
 def simple_delete(a_dictionary, key=""):
+    """ deletes a key in a dictionary """
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
