cb77369a4a4c3ef67ba00cb72f809306daf5829d
FranQ <frankchristopherquispe@gmail.com>
2022-06-15 01:02:46 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" Squared simple """
+
+
 def square_matrix_simple(matrix=[]):
-    return(list(list(map(lambda item: item ** 2, row)) for row in matrix))
+    """ computes the square value of all integers of a matrix """
+    return([list(map(lambda n: n ** 2, row)) for row in matrix])
