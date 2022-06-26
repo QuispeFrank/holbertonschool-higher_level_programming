ed52b1f37fa54fd474331d442218d366968562e1
FranQ <frankchristopherquispe@gmail.com>
2022-06-26 23:41:29 +0000

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+""" Raise a message """
+
+
 def raise_exception_msg(message=""):
+    """function that raises a name exception with a message.
+
+    Args:
+        message: string.
+    """
     raise NameError(message)
