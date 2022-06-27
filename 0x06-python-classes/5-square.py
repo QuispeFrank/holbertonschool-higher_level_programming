aeed966f3cf63f8cc043ba7018232870ed3b0fb0
FranQ <frankchristopherquispe@gmail.com>
2022-06-27 02:18:47 +0000

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-"""A square is here"""
+""" Printing a square """
 
 
 class Square:
-    """I'm a square class."""
+    """class Square.
+    """
 
     def __init__(self, size=0):
-        """Inits Square with size.
+        """ inits the square size.
 
-            args:
-                __size: size of the square.
+        Args:
+            size: (int) size of the square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
+
+    def area(self):
+        """area of the square.
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """get/set size of the square"""
+        """ returns square's size.
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """ set the square's size with validation.
+
+        Args:
+            value: square's size.
+        """
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = value
 
-    def area(self):
-        """Returns the Square's area"""
-        return (self.__size * self.__size)
-
     def my_print(self):
-        """prints a square"""
-        for row in range(self.__size):
-            [print("#", end="") for col in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")
+        """ print the square.
+        """
+        print((('#' * self.__size + '\n') * self.__size)[:-1])
