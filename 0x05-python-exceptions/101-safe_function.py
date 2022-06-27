95c687948247004ead176e5b7a468acc00694b6b
FranQ <frankchristopherquispe@gmail.com>
2022-06-27 00:09:42 +0000

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+""" Safe integer print with error message """
 import sys
 
 
 def safe_function(fct, *args):
+    """function that executes a function safely.
+
+    Args:
+        fct: a pointer to a function.
+        args: function's args.
+    """
     try:
         return(fct(*args))
     except BaseException as err:
-        sys.stderr.write("Exception: {}\n".format(err))
+        sys.stderr.write(f'Exception: {err}\n')
         return None
