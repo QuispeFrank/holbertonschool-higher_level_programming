4d7f29732ee48c807f3a38c4ef6574e9dfe91a6c
FranQ <frankchristopherquispe@gmail.com>
2022-07-06 22:53:14 +0000

    0x0E-SQL_more_queries$> Uploading 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- 0. My privileges!
+-- -------------------
+-- script that lists all privileges of the MySQL users user_0d_1
+-- and user_0d_2 on your server (in localhost).
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
