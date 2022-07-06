d8df2cca4de6f07d5b942b381a3d0e3bdb3d992e
FranQ <frankchristopherquispe@gmail.com>
2022-07-06 06:15:06 +0000

    0x0D. SQL - 13-change_class.sql

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- 13. Score too low
+-- -------------------
+-- script that removes all records with a score <= 5 in the table
+-- second_table of the database hbtn_0c_0 in your MySQL server.
+
+DELETE FROM second_table WHERE score <= 5
