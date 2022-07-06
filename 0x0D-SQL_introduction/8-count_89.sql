54aeb1c251e740380da16ad975d5be8baad26c6c
FranQ <frankchristopherquispe@gmail.com>
2022-07-06 04:18:30 +0000

    0x0D. SQL - 8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- 8. Count 89
+-- -------------
+-- script that displays the number of records with id = 89
+-- in the table first_table of the database.
+
+SELECT count(*) FROM first_table WHERE id = 89;
