2a675818eafb5607e5da2734ea3b9712e077b1f5
FranQ <frankchristopherquispe@gmail.com>
2022-07-06 03:59:19 +0000

    0x0D. SQL - 6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- 6. List all in table
+-- ----------------------
+-- script that lists all rows of the table first_table 
+-- from the database hbtn_0c_0 in your MySQL server.
+
+SELECT * FROM first_table;
