4037fcf01aaaa698b85069c9e0102fd445573054
FranQ <frankchristopherquispe@gmail.com>
2022-07-07 00:22:47 +0000

    0x0E-SQL_more_queries$> 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2. Read user
+-- --------------
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 will have only SELECT privilege in the database hbtn_0d_2.
+-- password for user_0d_2: user_0d_2_pwd.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
