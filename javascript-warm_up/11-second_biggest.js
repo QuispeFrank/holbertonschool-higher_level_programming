55a88825fff8c624cc860d41451ba9138017a8eb
FranQ <frankchristopherquispe@gmail.com>
2022-12-12 20:12:39 +0000

    Javascript Warm up <> 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const array = process.argv.slice(2).map(function (x) {
-  return Number(x);
-});
-array.sort(function (a, b) {
-  return a - b;
-});
-if (array.length > 1) {
+// script that searches the second biggest integer in the list of arguments.
+
+const array = process.argv.slice(2).sort(function (a, b) { return a - b; });
+
+if (array.length >= 2) {
   console.log(array[array.length - 2]);
 } else {
   console.log(0);
