be5921b34616741ceaa93202cb853e312b71bca8
FranQ <frankchristopherquispe@gmail.com>
2022-12-12 18:11:11 +0000

    Javascript Warm up <> 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const first = Number(process.argv[2]);
-const second = Number(process.argv[3]);
-console.log(`${first + second}`);
+// script that prints the addition of 2 integers
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+console.log(add(num1, num2));
+
+function add (a, b) {
+  return num1 + num2;
+}
