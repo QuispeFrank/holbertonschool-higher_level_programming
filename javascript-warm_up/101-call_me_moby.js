52380444c0bdb734ea45fa222cbe7ebe79325e75
FranQ <frankchristopherquispe@gmail.com>
2022-12-13 16:23:16 +0000

    Javascript Warm up <> 101-call_me_moby.js

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+// function that executes x times a function.
+
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) theFunction();
+  for (let counter = 0; counter < x; counter++) {
+    theFunction();
+  }
 };
