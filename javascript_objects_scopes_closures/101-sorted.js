5a90a129c1b2c0ddc34dc83ed147a9c3aa48e5f3
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 11:17:06 +0000

    JavaScript OSC <> 101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+// script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+
+const dict = require('./101-data').dict;
+const newdic = {};
+
+for (const key in dict) {
+  if (newdic[dict[key]] === undefined) {
+    newdic[dict[key]] = [key];
   } else {
-    newDict[dict[key]].push(key);
+    newdic[dict[key]].push(key);
   }
 }
-console.log(newDict);
+
+console.log(newdic);
