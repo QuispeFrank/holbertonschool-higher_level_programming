4922618b5994c0268f93369512d3359d0d85815f
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 06:09:04 +0000

    JavaScript OSC <> 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,23 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+// class Square that defines a square and inherits from Square of 5-square.js.
+
+const MainSquare = require('./5-square');
+
+class Square extends MainSquare {
+  constructor (side) {
+    super(side);
+    this.side = side;
+  }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      this.side = this.height;
+      for (let grow = 0; grow < this.side; grow++) {
+        console.log(c.repeat(this.side));
+      }
     }
   }
-};
+}
+module.exports = Square;
