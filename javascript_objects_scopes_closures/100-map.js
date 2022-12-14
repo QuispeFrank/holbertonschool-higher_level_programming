ae511a56fe66666dd3509b728083568b20597ff8
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 09:55:30 +0000

    JavaScript OSC <> 100-map.js

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+// script that imports an array and computes a new array.
+
+const list = require('./100-data').list;
+
+const newarray = list.map(function (element, index) {
+  return element * index;
+});
+
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newarray);
