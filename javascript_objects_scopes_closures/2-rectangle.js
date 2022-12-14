fa1872a1c3d6805f7640f4e0c09eaa3caeba85f5
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 01:54:51 +0000

    JavaScript OSC <> 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+// class Rectangle that defines a rectangle.
+
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w * h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
-};
+}
+
+module.exports = Rectangle;
