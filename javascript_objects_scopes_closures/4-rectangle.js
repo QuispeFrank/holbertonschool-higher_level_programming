61e3d421fc82ab2c9724cf30a3bbc875d53bcadf
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 09:26:09 +0000

    JavaScript OSC <> 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+// class Rectangle that defines a rectangle.
+
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w * h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let h = 0; h < this.height; h++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
   rotate () {
@@ -13,6 +20,8 @@ module.exports = class Rectangle {
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    [this.width, this.height] = [2 * this.width, 2 * this.height];
   }
-};
+}
+
+module.exports = Rectangle;
