a90e5d9e3b48919453d61c46b1dd3fbbdda4585e
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 12:53:07 +0000

    JavaScript OSC <> 102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,20 @@
 #!/usr/bin/node
+// script that concats 2 files.
+
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const file3 = process.argv[4];
+
+readAndappend(file1, file3);
+readAndappend(file2, file3);
+
+function readAndappend (filefrom, fileto) {
+  fs.readFile(filefrom, 'utf8', (err, text) => {
+    if (err) throw err;
+    fs.appendFile(fileto, text.toString(), (err) => {
+      if (err) throw err;
+    });
+  });
+}
