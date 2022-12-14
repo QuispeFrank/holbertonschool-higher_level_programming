bc572d1b4e8f3be597109b05d5aa48662067dad7
FranQ <frankchristopherquispe@gmail.com>
2022-12-14 09:18:29 +0000

    JavaScript OSC <> 10-converter.js

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-exports.converter = function (base) { return num => num.toString(base); };
+// function that converts a number from base 10 to another base passed as argument
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
