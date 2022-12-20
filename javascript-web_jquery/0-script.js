f824e1bb99616c45afe1d96edb76830484de2bef
FranQ <frankchristopherquispe@gmail.com>
2022-12-20 03:49:11 +0000

    JavaScript Web jQuery <> 0-script.js

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+/**
+ * JavaScript script that updates the text color of the <header>
+ * element to red (#FF0000) using document.querySelector.
+ */
+
+const FirstHeader = document.querySelector('header');
+FirstHeader.style.color = 'red';
