b1deaac8f118140c25774694c6687679443d2dae
FranQ <frankchristopherquispe@gmail.com>
2022-12-20 03:50:22 +0000

    JavaScript Web jQuery <> 3-script.js

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+/**
+ * JavaScript script that adds the class red to the <header>
+ * element when the user clicks on the tag DIV#red_header
+ */
+
+$('#red_header').click(() => {
+    $('header').addClass('red');
+})
